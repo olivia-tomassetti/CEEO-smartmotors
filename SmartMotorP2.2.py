33dea94d6c8674f59a829a1b0f0b540dfd5122c5
olivia-tomassetti <81169664+olivia-tomassetti@users.noreply.github.com>
2021-07-07 14:28:37 -0400

    Update SmartMotorP2.2.py

--- SmartMotorP2.2.py
+++ SmartMotorP2.2.py
@@ -1,92 +1,86 @@
+#Code to train Smart Motor with Buttons in place of potentiometer
 import machine
 import time
 from machine import Pin, ADC
 
 p2 = machine.Pin(0)
 pwm2 = machine.PWM(p2)
+pwm2.freq(50)
 
 pwm2.duty(90)
 
 #Initialize pins
 LED = Pin(5, Pin.OUT)
+BuiltInLED = Pin(2, Pin.OUT)
 LightSensor = ADC(0)
-#Buzzer = Pin(14, Pin.OUT)
 Left = Pin(13, Pin.IN) 
 Button = Pin(12, Pin.IN)
 Right = Pin(14, Pin.IN)
+print("about to enter")
 
-#t = [ [0]*5 for i in range(2)]
-
-run = 0
-count = 0
-
-while True: 
-    LED.off()
-    if run == 0:
-        while(Left.value() == True and pwm2.duty() < 138):
-            print("Left")
-            dutyValue = pwm2.duty() + 1
-            print(dutyValue)
-            pwm2.duty(dutyValue)
-            LED.on()
-            time.sleep(.05)
-            
-        while(Right.value() == True and pwm2.duty() > 26):
-            print("Right")
-            dutyValue = pwm2.duty() - 1
-            pwm2.duty(dutyValue)
-            print(dutyValue)
-            LED.on()
-            time.sleep(.05)
-        
-        if(Button.value()==True):
-            print("Button")
-            while(Button.value() == True):
-                time.sleep(.05)
-            dutyValue = pwm2.duty()
-            print(dutyValue)
-            lightValue = LightSensor.read()
-            print(lightValue)
-            if(count<0):
-                count = 0
-            elif(count>=0 and count<5):
-                training.append(dutyValue,lightValue)
-            else:
-                run = 1
-                while(Button.value() == False):
-                    LED.on()
-                    brightness = LightSensor.read()
-                    minimum = 1000
-                    pos = 0
-                    for (c,a,l) in training:
-                        dist = abs(brightness - training[i])
-                        if(dist<minimum):
-                            minimum = dist
-                            dutyValue = training[i]
-                    pwm2.duty(dutyValue)
-                    time.sleep(0.1)
-
-                count = 0
-                LED.off()
-
-            time.sleep(0.05)
-
-#Training loop
 while True:
-    LED.off()
-    while not Button.value:
-        time.sleep(.01)
-    angle = pwm2.duty()
-    bright = LightSensor.read()
-    buttonPress = 0
-    while  Button.value:
-        buttonPress += 1
-        if buttonPress == 20:
-            break
-        time.sleep(.1)
-    if buttonPress == 20:
-        break
-    training.append((angle,bright))
+    print("in loop")
+    training = []
+    #train for case A then B
+    for case in range(2):
+        LED.off()
+        BuiltInLED.on()
+        for counter in range(5):
+            while Button.value() == False:
+                time.sleep(.01)
+                while(Left.value() == True and pwm2.duty() < 138):
+                    print("Left")
+                    dutyValue = pwm2.duty() + 1
+                    print(dutyValue)
+                    pwm2.duty(dutyValue)
+                    LED.on()
+                    BuiltInLED.off()
+                    time.sleep(.05)
+                    BuiltInLED.on()
+                    LED.off()
+                while(Right.value() == True and pwm2.duty() > 26):
+                    print("Right")
+                    dutyValue = pwm2.duty() - 1
+                    pwm2.duty(dutyValue)
+                    print(dutyValue)
+                    LED.on()
+                    BuiltInLED.off()
+                    time.sleep(.05)
+                    LED.off()
+                    BuiltInLED.on()
+            print("Button pressed")
+            angle = pwm2.duty()
+            bright = LightSensor.read()
+            while  Button.value():
+                LED.on()
+                BuiltInLED.off()
+                time.sleep(.1)
+            LED.off()
+            BuiltInLED.on()
+            training.append((case,angle,bright))
+            print(angle)
+            print(bright)
 
-LED.off()
-print("training")
+
+    LED.on()
+    BuiltInLED.off()
+        
+    # grab reading
+    while True:
+        print("In the loop")
+        bright = LightSensor.read()
+        min = 1000
+        pos = 0
+        for (c,a,l) in training:
+            dist = abs(bright - l)
+            if dist < min:
+                min = dist
+                pos = a
+                val = c
+        pwm2.duty(pos)
+        time.sleep(.01)
+        if Button.value() == True:
+            break
+
+    LED.off()
+    BuiltInLED.on()
